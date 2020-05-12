7a3b973a0025c90334b5a5212eabb13ec74c3f96
Houssem.Romdhani <romdhanihoussem77@gmail.com>
2020-05-13 01:29:52 +0200

    Add project files.

---
+++ FullStackDemo.Front/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FullStackDemo.Front.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
